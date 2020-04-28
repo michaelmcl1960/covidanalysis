607140398974d4da53ca41388c76864850c72dbf
michaelmcl1960 <63645988+michaelmcl1960@users.noreply.github.com>
2020-04-28 19:40:46 +0100

    Add files via upload
    Added new plots

--- cvprocess.py
+++ cvprocess.py
@@ -15,6 +15,8 @@ import urllib.request as www
 covidDir='c:/gen/covid19'
 
 def cvprocess(covidDir):
+    IRLCasesToday=229          # if new data announced put here otherwise set to 0
+    IRLDeathsToday=59
     if not(os.path.isdir(covidDir)):
         try:
             os.mkdir(covidDir)
@@ -35,13 +37,13 @@ def cvprocess(covidDir):
         www.urlretrieve('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv',fname)
     else:
         print('File already retrieved')
-    IRLCasesToday=629+95         # if new data announced put here otherwise set to 0
-    IRLDeathsToday=43
-    nDaysLine=33            # No. of days to plot on line charts
+    nDaysLine=27            # No. of days to plot on line charts
     nDaysBar=14             # No. of days to plot on bar charts
     eurCountries=['AUT','BLR','BEL','BIH','BGR','HRV','CYP','DNK','EST','FIN','FRA','DEU','GRC','IRL',
                      'ITA','LUX','NLD','POL','PRT','ROU','SRB','SVK','SVN','ESP','SWE','CHE','GBR']
-    countries=['IRL','FRA',       'ITA',    'USA',          'ESP',  'GBR',   'DEU',           'AUS','SWE','EUR']
+    #countries=['AUS','JPN']
+    countries=['IRL','FRA',         'ITA',    'USA',  'ESP',       'GBR',      'SWE'       ]
+    countries=['IRL','SWE','ISL',         'ITA']
     colors=[[0,0.7,0],[0,0.25,0.85],[0,0.7,1],[1,0,0],[0.0,0.0,0.0],[1,0.7,0],[0.63,0.63,0],[0.85,0.9,0],[0.7,0.0,0.7],[0.0,0.55,0.70]]
     
     nCountries=len(countries)
@@ -68,14 +70,19 @@ def cvprocess(covidDir):
     allCNames = pd.DataFrame(columns=['CNames'])
     dayWidth=1      # width of each day
     countryWidth=dayWidth/(nCountries+2)
-    cms=['NewCases', 'NewDeaths', 'TotalCases', 'TotalDeaths','CaseFatalityRate','CaseRateIncrease','DeathRateIncrease','CasesPer1M', 'DeathsPer1M','Deaths Curve Shape (if DperM 20+)','DeathsPerM Since 10perM']
-    barchart=np.array([1 ,       1,          0,             0,             0,                  1,           1,           0,            0,            0,            0])
-    percent =np.array([0 ,       0,          0,             0,             1,                  1,           1,           0,            0,            1,            0])
+    cms=['NewCases', 'NewDeaths', 'TotalCases', 'TotalDeaths','CaseFatalityRate','CaseRateIncrease','DeathRateIncrease','DeathInc4DayAve','CasesPer1M', 'DeathsPer1M','Deaths Curve Shape (if DperM 20+)','DeathsPerM Since 10perM']
+    barchart=np.array([1 ,       1,          0,             0,             0,                  1,           1,           0,              0,            0,            0,            0])
+    percent =np.array([0 ,       0,          0,             0,             1,                  1,           1,           1,              0,            0,            1,            0])
     # Need an array of axes, there must be a better way to do it than this:
     fig, ax = plt.subplots(nrows=1, ncols=len(cms), figsize=(8, 20))
     plt.close(fig)
     for iAx in range(len(cms)):
-        plt.figure(iAx, figsize=(18, 10))
+        newf=plt.figure(iAx,figsize=(18, 10))
+        plt.gcf().canvas.set_window_title(cms[iAx])
+        if iAx>0:
+            fig=np.append(fig,newf)
+        else:
+            fig=newf
         ax[iAx]=plt.axes()
     nCmsTotal=len(cms)*len(countries)
     allData=np.zeros((maxDays,nCmsTotal))
@@ -107,6 +114,7 @@ def cvprocess(covidDir):
         roi=newCases[1:]/(cumCases[:-1]+1)
         rod=newDeaths[1:]/(cumDeaths[:-1]+1)
         rod[rod>1]=0;
+        rod4=np.convolve(rod,np.ones(4),mode='same')/4
         allData[-len(newCases):,iCol]=newCases
         allData[-len(newDeaths):,iCol+1]=newDeaths
         allData[-len(cumCases):,iCol+2]=cumCases
@@ -114,22 +122,22 @@ def cvprocess(covidDir):
         allData[-len(cumDeaths):,iCol+4]=cumDeaths/(cumCases+1)
         allData[-len(roi):,iCol+5]=roi
         allData[-len(rod):,iCol+6]=rod
-        allData[-len(cumCases):,iCol+7]=cumCases/pop*1000000
-        allData[-len(cumDeaths):,iCol+8]=cumDeaths/pop*1000000
-        #allData[-len(cumDeaths):,iCol+9]=cumDeaths/cumDeaths[-1:]
+        allData[-len(rod):,iCol+7]=rod4
+        allData[-len(cumCases):,iCol+8]=cumCases/pop*1000000
         allData[-len(cumDeaths):,iCol+9]=cumDeaths/pop*1000000
         allData[-len(cumDeaths):,iCol+10]=cumDeaths/pop*1000000
+        allData[-len(cumDeaths):,iCol+11]=cumDeaths/pop*1000000
         xBar=np.arange(nDaysBar)*dayWidth
         for iCms in range(len(cms)):
             if barchart[iCms]==1:
                 maxThis=np.max(allData[-nDaysBar:,iCol+iCms]);
                 ax[iCms].bar(xBar+iCountry*countryWidth+dayWidth/10,allData[-nDaysBar:,iCol+iCms],
                              countryWidth,label=countries[iCountry],color=colors[iCountry])
-            elif iCms!=10 and iCms!=9:
+            elif iCms!=11 and iCms!=10:
                 maxThis=np.max(allData[-nDaysLine:,iCol+iCms]);
                 ax[iCms].plot(dates[nDaysLine-1::-1],allData[-nDaysLine:,iCol+iCms],marker='o',
                               color=colors[iCountry],linewidth=3)
-            elif iCms==9:
+            elif iCms==10:
                  maxThis=1.0;
                  data=allData[-len(cumDeaths):,iCol+iCms]
                  if max(data)>20  and pop>3000000:            # Don't plot countries with <50 deaths per million
@@ -139,7 +147,7 @@ def cvprocess(covidDir):
                          ax[iCms].plot(np.arange(len(data))/len(data),data,marker='o',color=colors[iCountry],linewidth=3)
                  else:
                      ax[iCms].plot(nullxy,nullxy,'o',color=colors[iCountry],linewidth=3)
-            elif iCms==10:
+            elif iCms==11:
                  maxThis=np.max(allData[-nDaysLine:,iCol+iCms]);
                  data=allData[-len(cumDeaths):,iCol+iCms]
                  data=data[data>10]
@@ -164,7 +172,7 @@ def cvprocess(covidDir):
             axs[1]=nDaysBar
             ax[iAx].set_xticklabels(dates[nDaysBar-1::-daysPerLabelBar])
             ax[iAx].set_xticks(np.arange(0,nDaysBar,daysPerLabelBar))
-        elif iAx==9:
+        elif iAx==10:
             axs[1]=1
             ax[iAx].set_xticks(np.arange(0,1,0.1))
         else:
@@ -172,7 +180,7 @@ def cvprocess(covidDir):
             ax[iAx].set_xticks(np.arange(0,nDaysLine,daysPerLabelLine))
         if percent[iAx]==1:
             if maxy[iAx]<0.2:
-                ax[iAx].set_yticks(np.arange(0,maxy[iAx],0.02))
+                ax[iAx].set_yticks(np.arange(0,maxy[iAx],0.01))
             else:    
                 ax[iAx].set_yticks(np.arange(0,maxy[iAx],0.05))
             ax[iAx].yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1))
